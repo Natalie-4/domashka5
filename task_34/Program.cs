80e5ede368b67d820e2096a10b44d331c8876eab
Natalie <itlaym@yandex.ru>
2022-10-06 15:02:43 +0700

    solve task_34

--- task_34/Program.cs
+++ task_34/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами. 
+ Напишите программу, которая покажет количество чётных чисел в массиве.
+ [345, 897, 568, 234] -> 2 */
+
+Console.WriteLine("Введите число элементов в массиве: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] Array = new int[N];
+for (int i = 0; i < N; i++)
+{
+    Array[i] = new Random().Next(100,1000);
+}
+
+
+for (int i = 0; i < N; i++)
+{
+    Console.Write($"{Array[i]} ");
+}
+ Console.WriteLine();
+
+int SumEvenNumbers (int [] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < N; i++)
+{
+    if (Array[i] % 2 == 0)
+    {
+        sum = sum + Array[i];
+    }   
+}
+return sum;
+}
+
+Console.WriteLine($"Сумма чётных чисел массива равна {SumEvenNumbers(Array)}");
+
